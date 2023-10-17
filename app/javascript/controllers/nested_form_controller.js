8464a30f2b1187e9e67caebc43e28cf22d8cbb61
danielvec <vechioladaniel@gmail.com>
2023-10-16 18:00:24 -0700

    update nested form controller and add data-action to view to allow calculator to update total when removing fields

--- app/javascript/controllers/nested_form_controller.js
+++ app/javascript/controllers/nested_form_controller.js
@@ -13,6 +13,6 @@ export default class extends Controller {
     event.preventDefault();
     const item = event.target.closest(".nested-fields");
     item.querySelector("input[name*='_destroy']").value = 1;
-    item.style.display = 'none';
+    item.remove();
   }
 }
