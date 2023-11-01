43b95b966fa3b88bb6f6761465dfe4e430861ee1
danielvec <vechioladaniel@gmail.com>
2023-10-31 19:39:58 -0700

    add validations to require numericality for group fields

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :lift
 
-  validates :weight, presence: true
-  validates :reps, presence: true
+  validates :weight, presence: true, numericality: true
+  validates :reps, presence: true, numericality: true
 end
